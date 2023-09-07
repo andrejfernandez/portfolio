4766bd87050f7c08a582e912f405006c37a6b96a
Andre Fernandez <dev@andrejfernandez.io>
2023-09-06 22:26:46 -0400

    Updated structure

---
+++ src/utils/services.ts
@@ -0,0 +1,33 @@
+import placeholder from '/astro-logo-dark.svg';
+
+export interface Service {
+	id: number;
+	image: ImageMetadata;
+	title: string;
+	description: string;
+	tags: string[];
+}
+// array of 3 services using the service interface
+export const services: Service[] = [
+	{
+		id: 0,
+		image: placeholder,
+		title: 'Personal & Business Websites',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		tags: ['HTML', 'CSS', 'JavaScript', 'Svelte']
+	},
+	{
+		id: 1,
+		image: placeholder,
+		title: 'Portfolios & Blogs',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		tags: ['HTML', 'CSS', 'JavaScript', 'Svelte']
+	},
+	{
+		id: 2,
+		image: placeholder,
+		title: 'E-Commerce Websites',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		tags: ['HTML', 'CSS', 'JavaScript', 'Svelte']
+	}
+];
