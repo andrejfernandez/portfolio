da775ed0d325b2bc3554ece2a7afba747abe5e87
Andre Fernandez <dev@andrejfernandez.io>
2023-09-08 16:45:44 -0400

    Updated accessability

--- src/components/svelte/ContactForm.svelte
+++ src/components/svelte/ContactForm.svelte
@@ -11,25 +11,25 @@
 		<div class="flex justify-around">
 			<div class="form-control w-1/2 mx-5">
 				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<label class="label">
+				<label for="name" class="label">
 					<span class="label-text">Name</span>
 				</label>
-				<input type="text" class="input input-bordered w-full" />
+				<input id="name" type="text" class="input input-bordered w-full" />
 			</div>
 			<div class="form-control w-1/2 mx-5">
 				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<label class="label">
+				<label for="email" class="label">
 					<span class="label-text">Email</span>
 				</label>
-				<input type="text" class="input input-bordered w-full" />
+				<input id="email" type="text" class="input input-bordered w-full" />
 			</div>
 		</div>
 		<div class="form-control mx-5">
 			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<label class="label">
+			<label for="message" class="label">
 				<span class="label-text">Message</span>
 			</label>
-			<textarea class="textarea textarea-bordered h-40" />
+			<textarea id="message" class="textarea textarea-bordered h-40" />
 		</div>
 		<button class="btn btn-primary btn-wide mx-auto mt-10">submit</button>
 	</form>
